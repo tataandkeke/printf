9c92bc302e832fdc8af81cb2d5c9aae656e140a1
Rowland Oladiran <rowlandoladiran3@gmail.com>
2023-01-21 12:27:50 -0800

    Add

--- main/main.c
+++ main/main.c
@@ -9,17 +9,19 @@
 */
 int main(void)
 {
-	int len;
-	int len2;
+	/*int len;
+	int len2;*/
 	/*unsigned int ui;*/
 	/* void *addr; */
 
-	len = _printf("Let's try to printf a simple sentence.\n");
-	len2 = printf("Let's try to printf a simple sentence.\n");
+	/*len = _printf("Let's try to printf a simple sentence.\n");
+	len2 = printf("Let's try to printf a simple sentence.\n");*/
 	/*ui = (unsigned int)INT_MAX + 1024;*/
 	/*addr = (void *)0x7ffe637541f0;*/
+	_printf("Hey Test %d", 255);
+	/*
 	_printf("Length:[%d, %i]\n", len, len);
-	printf("Length:[%d, %i]\n", len2, len2);
+	printf("Length:[%d, %i]\n", len2, len2);*/
 	/*_printf("Negative:[%d]\n", -762534);
 	printf("Negative:[%d]\n", -762534);
 	_printf("Unsigned:[%u]\n", ui);
